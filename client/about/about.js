f9c6b33cf0bff09579cfad50b4a0a2affb2c01ae
Robert Dickert <robert.dickert@gmail.com>
2014-10-13 16:33:32 -0600

    Start to update the about page. Adding structure for contributors to add bio, photo, etc.

---
+++ client/about/about.js
@@ -0,0 +1,10 @@
+Template.about.helpers({
+  contributors:  [
+      {
+        name: "Robert Dickert",
+        bio: "Robert helped found Velocity by bringing many of the core team together at an online meetup about testing for Meteor Boulder. He is the author of the HTML reporter and organizes regular meetings of the core Velocity team. He is a Community Engineer at Meteor.",
+        photo: "images/team/robert_gh.jpg",
+        url: "http://robertdickert.com/"
+      }
+    ]
+});
