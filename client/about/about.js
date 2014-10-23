de18e35583e6eb307e2c7375f109e2e99fa2d4c4
alanning <alanning@gmail.com>
2014-10-23 13:37:35 -0400

    support gravatar, links in bio, and github repo. Add Adrian's bio

--- client/about/about.js
+++ client/about/about.js
@@ -5,6 +5,13 @@ Template.about.helpers({
         bio: "Robert helped found Velocity by bringing many of the core team together at an online meetup about testing for Meteor Boulder. He is the author of the HTML reporter and organizes regular meetings of the core Velocity team. He is a Community Engineer at Meteor.",
         photo: "images/team/robert_gh.jpg",
         url: "http://robertdickert.com/"
+      },
+      {
+        name: "Adrian Lanning",
+        bio: "Adrian was an early adopter of both Meteor and RTD, a predecessor to Velocity.  He contributes to Velocity core and the jasmine package and also maintains the <a href='https://atmospherejs.com/alanning/roles'>meteor-roles</a> package.  Adrian is CTO of <a href='https://share911.com/'>Share911</a>.",
+        photo: Gravatar.imageUrl("alanning@gmail.com"),
+        url: "https://share911.com/",
+        github: "https://github.com/alanning"
       }
     ]
 });
