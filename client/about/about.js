e3483608256a312da6591eb10172f851ea2beeb1
Michael Shilman <shilman@lab80.co>
2014-12-04 09:35:01 -0800

    alphabetized, updated Robert’s picture

--- client/about/about.js
+++ client/about/about.js
@@ -1,19 +1,6 @@
 Template.about.helpers({
   contributors:  [
       {
-        name: "Robert Dickert",
-        bio: "Robert helped found Velocity by bringing many of the core team together at an online meetup about testing for Meteor Boulder. He is the author of the HTML reporter and organizes regular meetings of the core Velocity team. He is a Community Engineer at Meteor.",
-        photo: "images/team/robert_gh.jpg",
-        url: "http://robertdickert.com/"
-      },
-      {
-        name: "Adrian Lanning",
-        bio: "Adrian was an early adopter of both Meteor and RTD, a predecessor to Velocity.  He contributes to Velocity core and the jasmine package and also maintains the <a href='https://atmospherejs.com/alanning/roles'>meteor-roles</a> package.  Adrian is CTO of <a href='https://share911.com/'>Share911</a>.",
-        photo: Gravatar.imageUrl("alanning@gmail.com"),
-        url: "https://share911.com/",
-        github: "https://github.com/alanning"
-      },
-      {
         name: "Jonas Aschenbrenner",
         bio: "Maintainer of sanjo:jasmine and velocity:core. Co-Founder of <a href='http://www.eventport.net/en/'>Eventport</a>. Enthusiastic Web Developer and Entrepreneur.",
         photo: Gravatar.imageUrl("jonas.aschenbrenner@gmail.com"),
@@ -21,11 +8,25 @@ Template.about.helpers({
         github: "https://github.com/Sanjo"
       },
       {
+        name: "Robert Dickert",
+        bio: "Robert helped found Velocity by bringing many of the core team together at an online meetup about testing for Meteor Boulder. He is the author of the HTML reporter and organizes regular meetings of the core Velocity team. He is a Community Engineer at Meteor.",
+        photo: Gravatar.imageUrl("robert.dickert@gmail.com"),
+        url: "http://robertdickert.com/"
+        github: "https://github.com/rdickert"
+      },
+      {
         name: "Sam Hatoum",
         bio: "Sam is an Agile consulting specializing in Continuous Delivery. He is the author of <a href='http://www.meteortesting.com'>The Meteor Testing Manual</a>, <a href='https://github.com/xolvio/meteor-cucumber'>meteor-cucumber</a>, <a href='https://github.com/xolvio/meteor-webdriver'>meteor-webdriver</a>, <a href='https://github.com/xolvio/meteor-coverage'>meteor-coverage</a> and the precursor to Velocity - <a href='http://rtd.xolv.io'>RTD</a>. Sam is also the founder of <a href='http://xolv.io'>Xolv.io</a> and a co-founder of <a href='http://placebuzz.com'>Placebuzz</a>",
         photo: Gravatar.imageUrl("sam@xolv.io"),
         url: "http://www.meteortesting.com/",
         github: "https://github.com/sam_hatoum"
+      },
+      {
+        name: "Adrian Lanning",
+        bio: "Adrian was an early adopter of both Meteor and RTD, a predecessor to Velocity.  He contributes to Velocity core and the jasmine package and also maintains the <a href='https://atmospherejs.com/alanning/roles'>meteor-roles</a> package.  Adrian is CTO of <a href='https://share911.com/'>Share911</a>.",
+        photo: Gravatar.imageUrl("alanning@gmail.com"),
+        url: "https://share911.com/",
+        github: "https://github.com/alanning"
       }
     ]
 });
