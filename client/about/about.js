3bcae3b0e83b77046733cb27d0d0b5d7bbd5321f
Michael Shilman <shilman@lab80.co>
2014-12-04 09:40:04 -0800

    typo

--- client/about/about.js
+++ client/about/about.js
@@ -11,7 +11,7 @@ Template.about.helpers({
         name: "Robert Dickert",
         bio: "Robert helped found Velocity by bringing many of the core team together at an online meetup about testing for Meteor Boulder. He is the author of the HTML reporter and organizes regular meetings of the core Velocity team. He is a Community Engineer at Meteor.",
         photo: Gravatar.imageUrl("robert.dickert@gmail.com"),
-        url: "http://robertdickert.com/"
+        url: "http://robertdickert.com/",
         github: "https://github.com/rdickert"
       },
       {
